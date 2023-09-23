d728d5b651904a03f4d4f35c51bdc27a7d59ebbb
Hayden Dekker <hayden.d@hotmail.com>
2023-09-23 20:24:22 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+const { app, BrowserWindow, BrowserView } = require('electron')
+const fs = require('fs')
+const path = require('path')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      webPreferences: {
+        webSecurity: false, // Allow loading insecure content
+        offscreen: true,
+    
+      },
+      show: false
+    })
+
+    //const bv = new BrowserView();
+    //win.setBrowserView(bv);
+
+    win.loadURL('https://hdekker.com');
+    //win.loadURL('https://192.168.8.99:8081/calendar');
+    win.webContents.on('paint', (event, dirty, image) => {
+        fs.writeFileSync('ex.png', image.toPNG())
+      })
+    win.webContents.setFrameRate(60)
+    console.log(`The screenshot has been successfully saved to ${path.join(process.cwd(), 'ex.png')}`)
+  }
+
+  // Disable certificate verification
+  app.commandLine.appendSwitch('ignore-certificate-errors'); 
+
+  app.whenReady().then(() => {
+    createWindow()
+  })

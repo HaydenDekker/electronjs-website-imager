c537e8bf9247425538b3c0cbe21627512efe7a7a
Hayden Dekker <hayden.d@hotmail.com>
2024-05-21 17:03:06 +1000

    docker build image

--- deploy/scripts/afterinstall.sh
+++ deploy/scripts/afterinstall.sh
@@ -1,26 +1,3 @@
 echo "after install"
 
-pwd 
-
-dirCurrent=${BASH_SOURCE%/*}
-
-echo "Searching for jar in $dirCurrent"
-
-jar_count=$(find $dirCurrent -type f -name "*.jar" | wc -l)
-
-if [[ $jar_count -eq 1 ]]; then
-  echo "Exactly one jar file found."
-else
-  if [[ $jar_count -eq 0 ]]; then
-    echo "No jar files found."
-  else
-    echo "More than one jar file found $jar_count."
-  fi
-  exit 1
-fi
-
-cd $dirCurrent
-
-cp *.jar install.jar
-
-docker build -t website-image-scheduler .
+docker build -t website-imager ../runtime_env

574b536a2d0e10a1a91efcafaa36a71b80dffcfc
Hayden Dekker <hayden.d@hotmail.com>
2024-06-12 19:30:42 +1000

    standardise default port to 8080

--- deploy/runtime_env/start_container.sh
+++ deploy/runtime_env/start_container.sh
@@ -1,3 +1,3 @@
 #/bin/bash
 
-docker run --privileged --detach -p 8084:8084 -v /home/hayden/Documents/website-imager:/app imager
+docker run --privileged --detach -p 8084:8080 -v /home/hayden/Documents/website-imager:/app imager

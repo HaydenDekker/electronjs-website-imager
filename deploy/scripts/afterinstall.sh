5f24c71a07768337868e33e1b366c5231ed2e69e
Hayden Dekker <hayden.d@hotmail.com>
2024-06-13 11:14:07 +1000

    Bug Fix
    Appspec copies all dist and node modules.
    Copies to local app folder. Folder probably not ideal.

--- deploy/scripts/afterinstall.sh
+++ deploy/scripts/afterinstall.sh
@@ -9,3 +9,9 @@ echo $script_dir
 cd $script_dir
 
 docker build -t website-imager ../runtime_env/
+
+cp -R dist ~/website-imager
+
+cp -R node_modules ~/website-imager
+
+cp -R package.json ~/website-imager

388ec3a2c852044f2e3af8c844973081fda2050a
Hayden Dekker <hayden.d@hotmail.com>
2024-06-13 11:55:12 +1000

    Nodejs version
    Update version to match docker build script for electron.

--- deploy/scripts/afterinstall.sh
+++ deploy/scripts/afterinstall.sh
@@ -10,8 +10,8 @@ cd $script_dir
 
 docker build -t website-imager ../runtime_env/
 
-cp -R dist ~/website-imager
+cp -R ../../dist ~/website-imager
 
-cp -R node_modules ~/website-imager
+cp -R ../../node_modules ~/website-imager
 
-cp -R package.json ~/website-imager
+cp -R ../../package.json ~/website-imager

1ec5f345f551ae68a5043e6cb8aaf09f9d010d24
Hayden Dekker <hayden.d@hotmail.com>
2024-06-19 14:05:00 +1000

    Updated node version and set chrome-sandbox privilege.

--- deploy/scripts/afterinstall.sh
+++ deploy/scripts/afterinstall.sh
@@ -11,5 +11,9 @@ cd $script_dir
 docker build -t website-imager ../runtime_env/
 
 sudo cp -R -f ../../dist ~/website-imager
+sudo cp -R -f ../../node_modules ~/website-imager
+
+sudo chown root ~/website-imager/node_modules/electron/dist/chrome-sandbox
+sudo chmod 4755 ~/website-imager/node_modules/electron/dist/chrome-sandbox
 
 sudo cp -f ../../package.json ~/website-imager

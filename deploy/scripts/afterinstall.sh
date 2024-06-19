888dddb7641be86b624fd32ee7b22c0c687f325e
Hayden Dekker <hayden.d@hotmail.com>
2024-06-19 15:24:15 +1000

    Deployment volume maintenance
    Removes old data, copies new files and sets permissions for docker.

--- deploy/scripts/afterinstall.sh
+++ deploy/scripts/afterinstall.sh
@@ -10,10 +10,19 @@ cd $script_dir
 
 docker build -t website-imager ../runtime_env/
 
+mkdir -p ~/website-imager
+
+echo "delete previous"
+sudo rm -R ~/website-imager/dist
+sudo rm -R ~/website-imager/node_modules
+sudo rm -R ~/website-imager/package.json
+
+echo "copy deployment files"
 sudo cp -R -f ../../dist ~/website-imager
 sudo cp -R -f ../../node_modules ~/website-imager
+sudo cp -f ../../package.json ~/website-imager
 
+echo "setting permissions"
+sudo chown -R 1000:1000 ~/website-imager
 sudo chown root ~/website-imager/node_modules/electron/dist/chrome-sandbox
 sudo chmod 4755 ~/website-imager/node_modules/electron/dist/chrome-sandbox
-
-sudo cp -f ../../package.json ~/website-imager
